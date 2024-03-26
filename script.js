14e01da190157eb522583f799024065befc76100
ROHAN PANDEY <rohan1706pandey@gmail.com>
2024-03-26 17:24:40 +0530

    Added linkedin-card, Updated resume-card

--- script.js
+++ script.js
@@ -61,3 +61,35 @@ if (cardElement) {
 } else {
   console.error("Card element not found!");
 }
+
+var linkedinCards = document.querySelectorAll(".linkedin-card");
+
+if (linkedinCards.length > 0) {
+  for (var i = 0; i < linkedinCards.length; i++) {
+    var linkedinCard = linkedinCards[i];
+
+    linkedinCard.addEventListener("mouseenter", function() {
+      var iconElement = this.querySelector(".fa.fa-linkedin");
+      if (iconElement) {
+        iconElement.classList.remove("fa-linkedin");
+        iconElement.classList.add("fa-linkedin-square");
+        iconElement.style.color = "#fff";
+        iconElement.style.fontSize = "70px";
+      } else {
+        console.error("Icon element not found inside card!", this);
+      }
+    });
+
+    linkedinCard.addEventListener("mouseleave", function() {
+      var iconElement = this.querySelector(".fa.fa-linkedin-square");
+      if (iconElement) {
+        iconElement.classList.remove("fa-linkedin-square");
+        iconElement.classList.add("fa-linkedin");
+        iconElement.style.color = "#ffffe4";
+        iconElement.style.fontSize = "48px"; 
+      }
+    });
+  }
+} else {
+  console.error("No card elements found with class 'linkedin-card'!");
+}
